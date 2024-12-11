b242a1bf0aa0372e2bb66d46e82514187047dbe3
Lukas Beyer <lukas.beyer@seznam.cz>
2024-12-11 20:59:23 +0100

    Create very basic logic for Task component

---
+++ components/Task.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    :class="[isCompleted ? 'bg-green-100' : 'bg-slate-100']"
+    class="rounded-full w-full p-6 hover:cursor-pointer"
+    @click="toggleCompleted"
+  >
+    {{ text }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  text: string;
+  completed: boolean;
+}>();
+
+const isCompleted = ref(props.completed);
+const toggleCompleted = () => {
+  isCompleted.value = !isCompleted.value;
+};
+</script>
+
+<style scoped lang="scss"></style>

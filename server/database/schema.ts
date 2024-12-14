3770a7aadc98587d11b80274649e8facaa984a32
Lukas Beyer <lukas.beyer@seznam.cz>
2024-12-14 18:02:37 +0100

    Add missing completed column in tasksTable

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -4,6 +4,7 @@ export const tasksTable = sqliteTable("tasks", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name").notNull(),
   description: text("description"),
+  completed: integer({ mode: "boolean" }).notNull().default(false),
   createdAt: integer("created_at", { mode: "timestamp" })
     .notNull()
     .default(new Date()),

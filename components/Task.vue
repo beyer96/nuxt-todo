f3a440b5e5224cce0963dfbd0ec4989ada8a5e26
Lukas Beyer <lukas.beyer@seznam.cz>
2024-12-15 09:24:23 +0100

    Improve Task component

--- components/Task.vue
+++ components/Task.vue
@@ -2,26 +2,69 @@
   <div
     :class="[
       isCompleted
-        ? 'bg-green-100 hover:bg-green-200 line-through'
+        ? 'bg-green-100 hover:bg-green-200'
         : 'bg-slate-100 hover:bg-slate-200',
     ]"
-    class="rounded-full w-full p-6 hover:cursor-pointer"
-    @click="toggleCompleted"
+    class="w-full p-6"
   >
-    {{ text }}
+    <div
+      class="flex items-center gap-2 hover:cursor-pointer"
+      :class="{ opened: descriptionOpened }"
+      @click="toggleDescription"
+    >
+      <button
+        class="w-8 h-8 border border-black hover:cursor-pointer grid place-items-center"
+        @click.stop="toggleCompleted"
+      >
+        <Icon
+          v-if="isCompleted"
+          name="mdi-light:check"
+          size="1.5rem"
+          class="font-bold"
+        />
+      </button>
+      <p class="font-semibold" :class="{ 'line-through': isCompleted }">
+        {{ task.name }}
+      </p>
+      <Icon
+        name="mdi-light:chevron-down"
+        size="2rem"
+        class="ms-auto"
+        :class="{ 'rotate-180': descriptionOpened }"
+        ref="chevronIcon"
+      />
+    </div>
+    <div class="description mt-4">
+      <p>{{ task.description }}</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Task } from "~/types/task";
+
 const props = defineProps<{
-  text: string;
-  completed: boolean;
+  task: Task;
 }>();
 
-const isCompleted = ref(props.completed);
+const isCompleted = ref(props.task.completed);
+const descriptionOpened = ref(false);
+
 const toggleCompleted = () => {
   isCompleted.value = !isCompleted.value;
 };
+const toggleDescription = () => {
+  descriptionOpened.value = !descriptionOpened.value;
+};
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.description {
+  display: none;
+
+  .opened ~ & {
+    display: block;
+    transition: 0.4s;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <Task :text="'Learn Nuxt'" :completed="true"></Task>
+  <div class="flex flex-col gap-4">
+    <Task v-for="task in tasks" :task="task" :key="task.id"></Task>
+  </div>
 </template>
 
 <script setup lang="ts">
+import type { Task } from "~/types/task";
+
+let tasks: Task[] | null;
+
 try {
   const { data, error } = await useFetch("/api/tasks");
   if (error.value) throw new Error(error.value.message);
 
   console.log(data.value);
+  tasks = data.value;
 } catch (error) {
   console.error(error);
 }
